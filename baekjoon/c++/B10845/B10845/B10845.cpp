c50a1a1cf75c6b700b8c9a6a9abbc3e96ca25795
[Hyunu97] <[at6jean3@gmail.com]>
2024-01-08 22:43:06 +0900

    HW_20240108_2 [Queue, Circular Linked List]

---
+++ baekjoon/c++/B10845/B10845/B10845.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+
+#define TRUE    1
+#define FALSE   0
+
+using namespace std;
+
+class MyQueue {
+private:
+    vector<int> _queue;
+    int _cursor;
+public: 
+    void Push(int x) {
+        _queue.push_back(x);
+    }
+
+    int Pop() {
+        if (this->Empty() == TRUE) {
+            return -1;
+        }
+
+        return _queue[_cursor++];
+    }
+
+    int Size() {
+        return _queue.size() - _cursor;
+    }
+
+    int Empty() {
+        return this->Size() == 0 ? TRUE : FALSE;
+    }
+
+    int Front() {
+        if (this->Empty() == TRUE) {
+            return -1;
+        }
+
+        return _queue[_cursor];
+    }
+
+    int Back() {
+        if (this->Empty() == TRUE) {
+            return -1;
+        }
+
+        return _queue.back();
+    }
+};
+
+int main()
+{
+    MyQueue queue;
+    int cnt;
+    cin >> cnt;
+    cin.ignore();
+
+    ostringstream oss;
+    for (int i = 0; i < cnt; i++) { 
+        string input;
+        getline(cin, input);
+        
+        istringstream iss(input);
+        vector<string> command(istream_iterator<string> {iss}, istream_iterator<string>());
+        if (command.size() != 1) {
+            queue.Push(stoi(command[1]));
+        } else {
+            if (command[0] == "pop") {
+                oss << queue.Pop() << endl;
+            } else if (command[0] == "size") {
+                oss << queue.Size() << endl;
+            } else if (command[0] == "empty") {
+                oss << queue.Empty() << endl;
+            } else if (command[0] == "front") {
+                oss << queue.Front() << endl;
+            } else if (command[0] == "back") {
+                oss << queue.Back() << endl;
+            } 
+        }
+
+    }
+    cout << oss.str();
+}
---
+++ baekjoon/c++/B1158/B1158/B1158.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+using namespace std;
+
+struct MyNode {
+    // current value
+    int _data;
+    // next element
+    MyNode* _next;
+    MyNode* _prev;
+    // ctor
+    MyNode(int data): _data(data), _prev(nullptr), _next(nullptr) {} // init Fields and next Node has null address
+};
+
+class MyLinkedList {
+private:
+    // first node
+    MyNode* _firstNode;
+    MyNode* _lastNode;
+   
+public:
+    MyLinkedList() : _firstNode(nullptr), _lastNode(nullptr) {}
+
+    void add(int data) {
+        MyNode* node = new MyNode(data);
+
+        if (_firstNode == nullptr) {
+            _firstNode = node;
+        } 
+
+        if (_lastNode == nullptr) {           
+            _lastNode = node;
+        }
+
+        // 1. Last node set NEXT node to the CURRENT node
+        this->_lastNode->_next = node;
+
+        // 2. Current node set PREVIOUS node to the CURRENT node
+        node->_prev = _lastNode;
+        
+        // !!!3. Current node set NEXT node to the FIRST node!!! (Circular Linked List)
+        node->_next = this->_firstNode;
+        this->_firstNode->_prev = node;
+
+        // 4. Update LAST node pointer
+        this->_lastNode = node;
+    }
+    
+    MyNode* GetFirstNode() {
+        return this->_firstNode;
+    }
+
+    MyNode* PopAtIndex(MyNode* node, int idx) {
+        for (int i = 0; i < idx; i++) {
+            node = node->_next;
+        }
+
+        MyNode* preNode = node->_prev;
+        MyNode* nextNode = node->_next;
+
+        preNode->_next = nextNode;
+        nextNode->_prev = preNode;
+
+        return node;
+    }
+};
+
+
+int main() {
+    MyLinkedList list;
+
+    string input;
+    getline(cin, input);
+    istringstream iss(input);
+    vector<string> command(istream_iterator<string> {iss}, istream_iterator<string>());
+
+    int capacity = stoi(command[0]);
+    int jump = stoi(command[1]);
+
+    for (int i = 0; i < capacity; i++) {
+        list.add(i + 1);
+    }
+
+
+    ostringstream oss;
+    oss << "<";
+    MyNode* node = list.GetFirstNode()->_prev;
+    for (int i = 0; i < capacity; i++) {
+        // append str
+        if (i != 0) oss << ", ";
+
+        node = list.PopAtIndex(node, jump);
+
+        oss << node->_data;
+    }
+    oss << ">";
+    cout << oss.str();
+}
