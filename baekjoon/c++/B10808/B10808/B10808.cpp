54011452fe4a18efcd4978a91624f57b4cdc7888
Hyunu97 <at6jean3@gmail.com>
2024-01-21 16:36:23 +0900

    20240121

---
+++ baekjoon/c++/B10808/B10808/B10808.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    vector<int> alpList(26, 0); // 알파벳 개수 자료구조 초기화
+    string input;
+    cin >> input;
+    // 97 ~ 122 아스키코드 a ~ z
+    for (char item : input) {
+        int index = item - 97;
+        alpList[index]++;
+    }
+
+    ostringstream oss;
+    for (int item : alpList) {
+        oss << item << " ";
+    }
+    cout << oss.str();
+}
---
+++ baekjoon/c++/B1918/B1918/B1918.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <stack>
+using namespace std;
+
+string calculate(stack<string> formula) {
+	stack<string> operations;
+	stack<string> alp;
+
+	ostringstream oss;
+	while (!formula.empty()) {
+		string b;
+		stack<string> formula2;
+		if (formula.top() == ")") {
+			formula.pop();
+
+			while (formula.top() != "(") {
+				formula2.push(formula.top());
+				formula.pop();
+			}
+			formula.pop();
+
+			b = calculate(formula2);
+		}
+
+		stack<string> formula3;
+		string operation = formula.top();
+		if (formula.top() == "*" || formula.top() == "/") {
+			formula3.push(b);
+			formula3.push(formula.top());
+			formula.pop();
+
+			formula3.push(formula.top());
+			formula.pop();
+
+			b += calculate(formula3);
+		}
+		else if (formula.top() == "+" || formula.top() == "-") {
+			b += formula.top();
+			formula.pop();
+		}
+		
+		ostringstream a;
+		oss << formula.top();
+		formula.pop();
+		oss << b;
+	}
+	
+	return oss.str();
+}
+
+int main()
+{
+	string input;
+	cin >> input;
+
+	stack<string> st;
+	// 제시된 계산식을 후입선출로 stack 자료구조에 다시 push
+	//for (int i = input.size() - 1; i >= 0; i--) {
+	//	string str(1, input[i]);
+	//	st.push(str);
+	//}
+
+	for (char item : input) {
+		string str(1, item);
+		st.push(str);
+	}
+
+	string result = calculate(st);
+
+	cout << result;
+}
