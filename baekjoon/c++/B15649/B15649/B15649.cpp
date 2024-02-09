4c555cc4e5631cf9e62d8310b2bd3ebd22e84b9c
Hyunu97 <at6jean3@gmail.com>
2024-02-09 23:07:03 +0900

    HW_20240209[Back_Tracking]

---
+++ baekjoon/c++/B15649/B15649/B15649.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10]; // 중복없이 고른 수열 (m)의 개수를 담아놓는 array
+bool isused[10]; // 재귀를 돌면서 선택된 숫자들은 true로 세팅
+
+void func(int k) {
+    if(k == m) { // 수열의 반복 회수에 도달하면 print
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!isused[i]) { // 사용하지 않는 상태면 재귀함수 호출
+            arr[k] = i;
+            isused[i] = true;
+            func(k + 1);
+            isused[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    func(0);
+}
---
+++ baekjoon/c++/B15652/B15652/B15652.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10]; // 중복없이 고른 수열 (m)의 개수를 담아놓는 array
+
+void func(int k, int idx) {
+    if (k > m) { // 수열의 반복 회수에 도달하면 print
+        for (int i = 1; i <= m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = idx; i <= n; i++) {
+        arr[k] = i;
+        func(k + 1, i);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    func(1, 1);
+}
